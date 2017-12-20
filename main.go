a228cc58970c83578c37baf74dbc2267e394b8b8
Kohei Hisakuni <kohei@philosophie.is>
2017-12-20 10:13:54 -0600

    Get user input

--- main.go
+++ main.go
@@ -2,23 +2,57 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("hey")
-	f, err := os.Open("./problems.csv")
+	csvFilename := flag.String("csv", "problems.csv", "a csv file in the fomrat of 'question, answer'")
+	flag.Parse()
+
+	file, err := os.Open(*csvFilename)
 	if err != nil {
-		panic(err)
+		exit(fmt.Sprintf("failed to open the CSV file: %s", *csvFilename))
 	}
-	r := csv.NewReader(f)
-	records, err := r.ReadAll()
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
 	if err != nil {
-		panic(err)
+		exit("Failed to parse provided CSV file.")
 	}
-	for _, record := range records {
-		fmt.Println(record)
+	problems := parseLines(lines)
+
+	correct := 0
+	for i, p := range problems {
+		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
+		var answer string
+		fmt.Scanf("%s\n", &answer)
+		if answer == p.a {
+			correct++
+		}
 	}
 
+	fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
+}
+
+func parseLines(lines [][]string) []problem {
+	ret := make([]problem, len(lines))
+	for i, line := range lines {
+		ret[i] = problem{
+			q: line[0],
+			a: strings.TrimSpace(line[1]),
+		}
+	}
+	return ret
+}
+
+type problem struct {
+	q string
+	a string
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
 }
